2fa9c0ddc7f2a74b9e0e0ef8cef923653a6f6d92
Dylan White <dylan.whitej@gmail.com>
2021-04-15 17:30:26 -0700

    FE rendering

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,14 @@
+
+// on page load, make a get request to verify endpoint to verify cookie
 fetch('/api/v1/auth/verify', {
     method: 'GET',
 }) 
 .then(res => {
+    // if the status is 200, parse into JSON and pass along, if not throw new error
     if (res.ok) return res.json();
     else throw new Error('Not Logged In!');
 })
+// take user info and render on screen with form
 .then(json => renderLoggedIn(json))
 .catch(() => {
     const root = document.getElementById('root');
@@ -15,3 +19,20 @@ fetch('/api/v1/auth/verify', {
     button.textContent = "Log in with Github";
     root.appendChild(button);
 })
+
+renderLoggedIn = (user) => {
+    const root = document.getElementById('root');
+    const nameHeader = document.createElement('h1');
+    const form = document.createElement('form');
+    const text = document.createElement('textarea');
+    const button = document.createElement('button');
+
+    button.textContent = "Post";
+    nameHeader.textContent = user.username;
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+    });
+    form.appendChild(text, button);
+    root.appendChild(nameHeader, form);
+};
