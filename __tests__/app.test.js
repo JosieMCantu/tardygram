40b6a0163475ce240292cbdb655cf3b071899b64
JosieMCantu <josiex2@gmail.com>
2021-04-16 15:08:15 -0700

    adds GREEN post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,36 +2,46 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
+
+jest.mock('../lib/middleware/ensureAuth.js', () => (req, res, next) => {
+  req.user = { userName: 'testUser', avatar: 'http://www.me.com' };
+  next();
+});
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  jest.mock('../lib/middleware/ensureAuth.js', () => (req, res,next) => {
-    req.user = {userName: 'testUser', avatar: 'http://www.me.com'}
-    next()
+  beforeEach(async () => {
+    await User.insert({
+      username: 'testUser',
+      photo: 'http:www.me.com'
+    })
   })
 
-const testUser = { 
-          userName: 'testUser', 
-          caption: 'whatever', 
-          photo_url: 'http:www.me.com', 
-          tags: ['nice', 'this', 'that']
-        }
-        
+  const testUser = {
+    userName: 'testUser',
+    caption: 'whatever',
+    photoUrl: 'http:www.me.com',
+    tags: ['nice', 'this', 'that']
+  }
+
 
   it('POST should post a new post to the posts database', async () => {
     const response = await request(app)
-    .post('/api/v1/auth/posts')
-    .send({ 
-      userName: 'testUser', 
-      caption: 'whatever', 
-      photo_url: 'http:www.me.com', 
-      tags: ['nice', 'this', 'that']})
+      .post('/api/v1/post')
+      .send({
+        userName: 'testUser',
+        caption: 'whatever',
+        photoUrl: 'http:www.me.com',
+        tags: ['nice', 'this', 'that']
+      })
 
-      const expectation = {
-        id: 1,
-        ...testUser}
+    const expectation = {
+      postId: '1',
+      ...testUser
+    }
 
     expect(response.body).toEqual(expectation)
   })
